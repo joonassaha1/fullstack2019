b790ef82972584d9f452fc61a3c86c77572615cc
Joonas Saha <joonassaha@Joonass-MacBook-Pro.local>
2019-01-28 22:11:28 +0200

    1.5 step valmis

--- osa1uusi/src/index.js
+++ osa1uusi/src/index.js
@@ -1,77 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const Header = (props) => {
     console.log(props)
-    return(
+
+    return (
         <div>
             <h1>{props.course}</h1>
         </div>
     )
 }
 
-const Content = (props) => {
+const Part = (props) => {
     console.log(props)
+
     return (
         <div>
-            <p>{props.name1} {props.exercises1}</p>
-            <p>{props.name2} {props.exercises2}</p>
-            <p>{props.name3} {props.exercises3}</p>
+            <p>
+            {props.part.name} {props.part.exercises}
+            </p>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    console.log(props)
+
+    return (
+        <div>
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
         </div>
     )
 }
 
 const Total = (props) => {
     console.log(props)
+
     return (
         <div>
-            <p>Yhteensä {props.amount} tehtävää</p>
+            <p>yhteensä {props.parts[0].exercises + 
+            props.parts[1].exercises + 
+            props.parts[2].exercises} tehtävää</p>
         </div>
     )
 }
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-
-/*     const parts = [
-        {
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+          {
             name: 'Reactin perusteet',
             exercises: 10
-        },
-        {
+          },
+          {
             name: 'Tiedonvälitys propseilla',
             exercises: 7
-        },
-        {
+          },
+          {
             name: 'Komponenttien tila',
             exercises: 14
-        } 
-    ] */
-    const part1 = {
-        name: 'Reactin perusteet',
-        exercises: 10
+          }
+        ]
     }
-    const part2 = {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'Komponenttien tila',
-        exercises: 14
-    }
+  
 
-    const maara = part1.exercises + part2.exercises + part3.exercises
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
 
-    return (
-        <div>
-            <Header course = {course} />
-            <Content name1 = {part1.name} exercises1 = {part1.exercises}/>
-            <Content name2 = {part2.name} exercises2 = {part2.exercises}/>
-            <Content name3 = {part3.name} exercises3 = {part3.exercises}/>
-            <Total amount = {maara}/>
-        </div>
-    )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'))
